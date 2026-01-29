a9f9dd58596ca6d7c054e92c2bfcd267a4fff092
Samuel <soccersha120@gmail.com>
2026-01-29 12:47:56 +0800

    Implement Auth UI & Layout

---
+++ components/forms/FooterLink.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const FooterLink = ({text, linkText, href}: FooterLinkProps) => {
+  return (
+    <div className="text-center pt-4">
+        <p className="text-sm text-gray-500">
+            {text}{' '}
+            <a href={href} className="footer-link">
+                {linkText}
+            </a>
+        </p>
+    </div>
+  )
+}
+
+export default FooterLink
